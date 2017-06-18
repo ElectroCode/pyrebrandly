3f80878aeed7b1588960bc03d4729c5fcb7e488b
Ken Spencer <ken@electrocode.net>
2017-06-18 10:32:29 -0400

    Initial Commit

---
+++ pyrebrandly/conf.py
@@ -0,0 +1,12 @@
+# pyrebrandly/conf.py
+# Ken Spencer <ken@electrocode.net>
+#
+# This module is part of pyrebrandly and is released under the
+# MIT license: http://opensource.org/licenses/MIT
+
+#
+from orderedattrdict import AttrDict
+
+class Configuration(AttrDict):
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rebrandly',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/ElectroCode/pyrebrandly',
+    license='MIT',
+    author='Ken Spencer',
+    author_email='ken@electrocode.net',
+    description='Interfaces with the Rebrandly.com API'
+)
