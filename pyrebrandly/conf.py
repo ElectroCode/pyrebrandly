b6182d2fc03cfd32e2dbe34aeba3d08201558f28
Ken Spencer <ken@electrocode.net>
2017-06-29 09:50:13 -0400

    pyrebrandly: start porting over ruby code

--- pyrebrandly/conf.py
+++ pyrebrandly/conf.py
@@ -1,12 +1,1 @@
-# pyrebrandly/conf.py
-# Ken Spencer <ken@electrocode.net>
-#
-# This module is part of pyrebrandly and is released under the
-# MIT license: http://opensource.org/licenses/MIT
-
-#
-from orderedattrdict import AttrDict
-
-class Configuration(AttrDict):
-
 
