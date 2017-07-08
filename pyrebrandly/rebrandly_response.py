d7df5dd5a446a9b1882a4586441bdde2dd364fe6
Ken Spencer <ken@electrocode.net>
2017-07-08 15:45:13 -0400

    rebrandly_response: import * instead of RebrandlyError
    * This is due to making all exceptions part of the base of pyrebrandly.exceptions
    * This means exceptions are pyrebrandly.exceptions.*Error

--- pyrebrandly/rebrandly_response.py
+++ pyrebrandly/rebrandly_response.py
@@ -1,4 +1,4 @@
-from pyrebrandly.exceptions import RebrandlyError
+from pyrebrandly.exceptions import *
 
 
 class RebrandlyResponse:
@@ -9,21 +9,21 @@ class RebrandlyResponse:
             return {'status': 'ok', 'response': rebrandly_response}
             # Everything went well, continue.
         elif code == 400:
-            raise RebrandlyError.BadRequestError(rebrandly_response.message)
+            raise BadRequestError(rebrandly_response.message)
         elif code == 401:
-            raise RebrandlyError.NotAuthorizedError(rebrandly_response.message)
+            raise NotAuthorizedError(rebrandly_response.message)
         elif code == 403:
             if rebrandly_response.code == 'AlreadyExists':
-                raise RebrandlyError.AlreadyExistsError(rebrandly_response.message)
+                raise AlreadyExistsError(rebrandly_response.message)
             else:
-                raise RebrandlyError.InvalidFormatError(rebrandly_response.message)
+                raise InvalidFormatError(rebrandly_response.message)
         if code == 404:
-            raise RebrandlyError.NotFoundError(rebrandly_response.message)
+            raise NotFoundError(rebrandly_response.message)
         if code == 500:
-            raise RebrandlyError.InternalServerError(rebrandly_response.message)
+            raise InternalServerError(rebrandly_response.message)
         if code == 502:
-            raise RebrandlyError.BadGatewayError(rebrandly_response.message)
+            raise BadGatewayError(rebrandly_response.message)
         if code == 503:
-            raise RebrandlyError.APIUnavailableError(rebrandly_response.message)
+            raise APIUnavailableError(rebrandly_response.message)
         if code == 504:
-            raise RebrandlyError.APITimeoutError(rebrandly_response.message)
+            raise APITimeoutError(rebrandly_response.message)
