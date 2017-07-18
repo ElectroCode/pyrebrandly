3d87017d72043145039c3060d9cc0ef643eea9a7
Ken Spencer <ken@electrocode.net>
2017-07-18 08:11:55 -0400

    ./init: make users import pyrebrandly.api instead of pyrebrandly, comment out the config since we use it differently

--- pyrebrandly/__init__.py
+++ pyrebrandly/__init__.py
@@ -1,17 +1,11 @@
-from .api import *
-from .exceptions import *
+
 
 __all__ = ()
 
 __version__ = '0.8.1'
 
 
-from orderedattrdict import DefaultAttrDict
-tree = lambda: DefaultAttrDict(tree)
-
-config = tree()
-config.api.key = None
-config.domain.id = None
-config.domain.name = None
-config.team.id = None
-
+# from orderedattrdict import DefaultAttrDict
+# tree = lambda: DefaultAttrDict(tree)
+#
+# config = tree()
